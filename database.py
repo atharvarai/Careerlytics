bc56e6d515550d2a630b9cc9ec5cf3353e3fb880
Atharva Rai <atharvarai78@gmail.com>
2024-12-26 16:15:04 +0530

    added route for contact_us page

--- database.py
+++ database.py
@@ -9,7 +9,6 @@ engine = create_engine(db_connection_string)
 def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("select * from jobs"))
-        # Convert each row to a dictionary using _mapping
         jobs = [row._mapping for row in result]
         return jobs
 
@@ -17,13 +16,12 @@ def load_job_from_db(id):
     with engine.connect() as conn:
         result = conn.execute(
             text("SELECT * FROM jobs WHERE id = :val"),
-            {"val": id}  # Use a dictionary for parameters
+            {"val": id}
         )
         rows = result.all()
         if len(rows) == 0:
             return None
         else:
-            # Convert to dictionary using _mapping
             return rows[0]._mapping
 
 def add_application_to_db(job_id, data):
@@ -34,7 +32,6 @@ def add_application_to_db(job_id, data):
             VALUES 
             (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)
         """)
-        # Create a parameters dictionary
         params = {
             "job_id": job_id,
             "full_name": data['full_name'],
@@ -43,6 +40,23 @@ def add_application_to_db(job_id, data):
             "education": data['education'],
             "work_experience": data['work_experience'],
             "resume_url": data['resume_url']
+        }
+        conn.execute(query, params)
+        conn.commit()
+
+def add_message_to_db(data):
+    with engine.connect() as conn:
+        query = text("""
+            INSERT INTO messages 
+            (name, email, message, phone_no) 
+            VALUES 
+            (:name, :email, :message, :phone_no)
+        """)
+        params = {
+            "name": data['txtName'],
+            "email": data['txtEmail'],
+            "message": data['txtMsg'],
+            "phone_no": data['txtPhone']
         }
         conn.execute(query, params)
         conn.commit()
